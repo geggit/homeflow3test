82fba1a6e056d62cfc9bd461061635cdfd6bf2e7
geggit <ronbeli@hotmail.com>
2013-08-13 06:32:29 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Homeflow3test::Application.config.secret_key_base = '02ef83cd8e60358ad2b766c27180481fa4d6f64047844cbd6337d303a87434bb92e6b65bdfa80a98ffde242970c3c0496ecc4e92e0a780679d95d1152fe56bf0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Homeflow3test::Application.config.session_store :cookie_store, key: '_homeflow3test_session'
